87ab89b55fb8f2d94842e5d590d4c62571152f97
nicerjs23 <nicerjs23@gmail.com>
2026-02-21 23:20:36 +0900

    Feat: 스플래시페이지구현

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HeaderLayout from '@components/Layout/HeaderLayout';
 import FullLayout from '@components/Layout/FullLayout';
 // 페이지 컴포넌트
-import Splash from '@pages/Splash';
+import Splash from '@/pages/Splash';
 import Main from '@pages/Main';
 import BoothMap from '@pages/BoothMap';
 import Test from '@pages/Test';
@@ -13,19 +13,17 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route path='/' element={<Splash />} />
         {/* 전체 화면 레이아웃 */}
         <Route element={<FullLayout />}>
-          <Route path="/" element={<Splash />} />
-          <Route path="/main" element={<Main />} />
+          <Route path='/main' element={<Main />} />
         </Route>
 
         {/* 헤더 있는 화면 레이아웃 */}
         <Route element={<HeaderLayout />}>
-          <Route path="/boothmap" element={<BoothMap />} />
-          <Route path="/test" element={<Test />} />
+          <Route path='/boothmap' element={<BoothMap />} />
+          <Route path='/test' element={<Test />} />
         </Route>
-
       </Routes>
     </BrowserRouter>
   );
