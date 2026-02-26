d55289b69cf5ef54ea12c0b2bf6d44347b43de5e
leeboyeon17 <2023110695@dgu.ac.kr>
2026-02-27 02:16:50 +0900

    fix: 타입 수정, useBooths 필수 파라미터 추가

--- src/utils/boothUtils.ts
+++ src/utils/boothUtils.ts
@@ -13,7 +13,7 @@ export const getDivisionFromBooths = (booths:any[]): string[] => {
     // 4. filter(Boolean)으로 null이나 빈 문자열 제거
     // => 전체 부스에서 유효한 분과명 데이터 추출 완료
 
-    return Array.from(new Set(booths.map((booth) => booth.division))).filter(
+    return Array.from(new Set(booths.map((booth) => booth.division_name))).filter(
         (division): division is string => !!division
     );
 };
